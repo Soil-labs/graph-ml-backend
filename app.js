58bb941341b87b53dd8ba482c22af56b8c4866a1
BluePanda <m.saratzidis@gmail.com>
2022-12-23 18:28:21 +0100

    feat/changeLog

--- app.js
+++ app.js
@@ -1,49 +1,41 @@
-
 import dotenv from "dotenv";
-import {findProjects,findSkillsToRecalculate,matchPrepareSkillToMembers,matchPrepareSkillToProjectRoles} from "./backEnd_api_func.js"
-
+import {
+  findProjects,
+  findSkillsToRecalculate,
+  matchPrepareSkillToMembers,
+  matchPrepareSkillToProjectRoles,
+} from "./backEnd_api_func.js";
 
 dotenv.config();
 
+console.log("I am alive!");
 
-console.log("I am alive!")
+const interval = setInterval(async function () {
+  console.log(" --- Search for New Recalculate Skills --- ");
 
+  let res = await findSkillsToRecalculate();
 
+  console.log("number of skills to search: = ", res.length());
 
+  for (let i = 0; i < res.length; i++) {
+    findMatchToSkillForProject(res[i]);
 
+    console.log("change = ", i, res.length);
+  }
+}, 30000);
 
-const interval = setInterval( async function() {
-    console.log(" --- Search for New Recalculate Skills --- " )
+interval;
 
-    let res = await findSkillsToRecalculate()
-
-
-    for (let i = 0; i < res.length; i++) {
-
-        findMatchToSkillForProject(res[i])
-
-        console.log("change = ",i,res.length )
-
-    }
-  }, 30000);
- 
-interval 
-
-
-    // console.log("res findSkillsToRecalculate= " )
-
-
+// console.log("res findSkillsToRecalculate= " )
 
 async function findMatchToSkillForProject(skillData) {
+  // if (skillData._id.toString() == ("63098cfbb003e10004f9a9f1")) {
+  console.log("skillData = ", skillData);
 
-    // if (skillData._id.toString() == ("63098cfbb003e10004f9a9f1")) {
-        console.log("skillData = ",skillData )
+  let res = await matchPrepareSkillToMembers(skillData._id);
+  console.log("matchPrepareSkillToMembers = ", res);
 
-        let res = await matchPrepareSkillToMembers(skillData._id)
-        console.log("matchPrepareSkillToMembers = " , res)
-
-        let res2 = await matchPrepareSkillToProjectRoles(skillData._id)
-        console.log("matchPrepareSkillToProjectRoles = " , res2)
-    // }
-    
+  let res2 = await matchPrepareSkillToProjectRoles(skillData._id);
+  console.log("matchPrepareSkillToProjectRoles = ", res2);
+  // }
 }
