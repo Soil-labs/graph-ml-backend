868904c6ccd8fd11996e6158a8cffd0a5a570dd6
BluePanda <m.saratzidis@gmail.com>
2022-12-23 21:12:39 +0100

    feat/clean_log_AI2

--- app.js
+++ app.js
@@ -1,49 +1,43 @@
-
 import dotenv from "dotenv";
-import {findProjects,findSkillsToRecalculate,matchPrepareNode,findNodesToRecalculate,matchPrepareSkillToMembers,matchPrepareSkillToProjectRoles} from "./backEnd_api_func.js"
-
+import {
+  findProjects,
+  findSkillsToRecalculate,
+  matchPrepareNode,
+  findNodesToRecalculate,
+  matchPrepareSkillToMembers,
+  matchPrepareSkillToProjectRoles,
+} from "./backEnd_api_func.js";
 
 dotenv.config();
 
+console.log("I am alive!");
 
-console.log("I am alive!")
+const interval = setInterval(async function () {
+  console.log(" --- Search for New Recalculate Skills --- ");
 
+  let res = await findNodesToRecalculate();
 
+  console.log("num res = ", res.length);
 
+  for (let i = 0; i < res.length; i++) {
+    findMatchToSkillForProject(res[i]);
 
+    // console.log("change = ",i,res.length )
+  }
+}, 20000);
 
-const interval = setInterval( async function() {
-    console.log(" --- Search for New Recalculate Skills --- " )
+interval;
 
-    let res = await findNodesToRecalculate()
-
-
-    for (let i = 0; i < res.length; i++) {
-
-        findMatchToSkillForProject(res[i])
-
-        console.log("change = ",i,res.length )
-
-    }
-  }, 15000);
- 
-interval 
-
-
-    // console.log("res findSkillsToRecalculate= " )
-
-
+// console.log("res findSkillsToRecalculate= " )
 
 async function findMatchToSkillForProject(nodeData) {
+  // if (nodeData._id.toString() == ("63098cfbb003e10004f9a9f1")) {
+  // console.log("nodeData = ", nodeData);
 
-    // if (nodeData._id.toString() == ("63098cfbb003e10004f9a9f1")) {
-        console.log("nodeData = ",nodeData )
+  let res = await matchPrepareNode(nodeData._id, "Member");
+  // console.log("matchPrepareSkillToMembers = ", res);
 
-        let res = await matchPrepareNode(nodeData._id,"Member")
-        console.log("matchPrepareSkillToMembers = " , res)
-
-        let res2 = await matchPrepareNode(nodeData._id,"Role")
-        console.log("matchPrepareSkillToProjectRoles = " , res2)
-    // }
-    
+  let res2 = await matchPrepareNode(nodeData._id, "Role");
+  // console.log("matchPrepareSkillToProjectRoles = " , res2)
+  // }
 }
