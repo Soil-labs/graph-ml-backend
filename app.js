3b698244d4f6d9c6709660df7ae0543722c5ec3e
BluePanda <m.saratzidis@gmail.com>
2023-01-02 15:30:29 +0100

    feat/AI3.8_fasterUpdate_variable

--- app.js
+++ app.js
@@ -15,41 +15,107 @@ console.log("I am alive!");
 let max_num_updates = 1;
 let posUpdate = 0;
 
-const interval = setInterval(async function () {
-  console.log(" --- Search for New Recalculate Skills --- ");
+let timeRepeatSlow = 6000;
+let timeRepeatFast = 1000;
+let timeRepeat = timeRepeatSlow;
+
+function changeInterval(res_length) {
+  if (res_length > 0) {
+    timeRepeat = timeRepeatFast;
+  } else {
+    timeRepeat = timeRepeatSlow;
+  }
+}
+
+// const interval = setInterval(async function () {
+//   console.log(" --- Search for New Recalculate Skills --- ");
+
+//   let res = await findNodesToRecalculate("All");
+
+//   if (res) {
+//     res = res.data.data.findNodes;
+//     console.log(" num res =  ", res.length);
+//     // console.log(" posUpdate =  ", posUpdate);
+
+//     changeInterval(res.length);
+
+//     console.log("timeRepeat = ", timeRepeat);
+
+//     // if (res.length > 0) {
+//       // if (res.length > max_num_updates) {
+//       //   posUpdate = Math.floor(Math.random() * (res.length - max_num_updates));
+//       // }
+
+//       // // posUpdate = 86;
+//       // // max_num_updates = 1;
+
+//       // if (posUpdate + max_num_updates > res.length) {
+//       //   max_num_updates = res.length - posUpdate;
+//       // }
+
+//       // for (let i = posUpdate; i < posUpdate + max_num_updates; i++) {
+//       //   // for (let i = 0; i < res.length; i++) {
+//       //   findMatchToSkillForProject(res[i]);
+//       //   console.log("res[i]._id = ", i, res[i]._id);
+//       //   // console.log("change = ",i,res.length )
+//       // }
+
+//     // }
+//   }
+
+//   // posUpdate = posUpdate + max_num_updates;
+// }, timeRepeat);
+
+// interval;
+
+var speed_fast = 4000;
+var speed_slow = 30000;
+
+var speed = speed_fast;
+var speed_before = speed_fast;
+var changeSpeed = speed_fast;
+
+let repeater;
+repeater = setInterval(repeaterFn, speed);
+
+async function repeaterFn() {
+  console.log("changeSpeed,speed_before = ", changeSpeed, speed_before);
+  if (changeSpeed != speed_before) {
+    console.log("change = 1.1.1");
+    clearInterval(repeater);
+    speed_before = changeSpeed;
+    speed = changeSpeed;
+    repeater = setInterval(repeaterFn, speed);
+  }
 
   let res = await findNodesToRecalculate("All");
-
   if (res) {
     res = res.data.data.findNodes;
-    console.log(" num res =  ", res.length);
-    // console.log(" posUpdate =  ", posUpdate);
-
+    console.log("res.length = ", res.length);
     if (res.length > 0) {
+      changeRepeater(speed_fast);
+
       if (res.length > max_num_updates) {
         posUpdate = Math.floor(Math.random() * (res.length - max_num_updates));
       }
-
-      // posUpdate = 86;
-      // max_num_updates = 1;
 
       if (posUpdate + max_num_updates > res.length) {
         max_num_updates = res.length - posUpdate;
       }
 
       for (let i = posUpdate; i < posUpdate + max_num_updates; i++) {
-        // for (let i = 0; i < res.length; i++) {
         findMatchToSkillForProject(res[i]);
         console.log("res[i]._id = ", i, res[i]._id);
-        // console.log("change = ",i,res.length )
       }
+    } else {
+      changeRepeater(speed_slow);
     }
   }
-
-  // posUpdate = posUpdate + max_num_updates;
-}, 22000);
-
-interval;
+}
+function changeRepeater(speed_change) {
+  console.log("change = 0.0.0");
+  changeSpeed = speed_change;
+}
 
 // console.log("res findSkillsToRecalculate= " )
 
