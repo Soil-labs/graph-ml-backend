f0016c89c2f5bc2814973752e6aaa561b5302d99
James Oshomah <jamiebones2000@yahoo.co.uk>
2023-06-14 10:47:41 +0100

    added subscription to the codebase

--- backEnd_api_func.js
+++ backEnd_api_func.js
@@ -1,5 +1,6 @@
 import apiClient from "./api/axios.js";
 import apiClientCron from "./api/axiosCron.js";
+import subscriptionClient from "./api/subscriptionClient.js";
 // import apiClientCron from "./api/axiosCron.js";
 
 export async function findProjects() {
@@ -387,3 +388,25 @@ export async function relatedSkills(coreSkill_id, relatedSkill_id) {
   // return (res.data)
   return res.data.data.relatedSkills;
 }
+
+export async function autoUpdateUserInfoFromCVSubsccriptionVersion( userID ) {
+  let res = await apiClientCron({
+    data: {
+      query: `mutation{
+            autoUpdateUserInfoFromCV(fields:{userIDs: [${userID}]}){
+              users {
+                _id
+                discordName
+                bio
+              }
+            }
+          }`,
+    },
+  }).catch((err) => {
+    console.log(err);
+  });
+
+  return res.data.data.autoUpdateUserInfoFromCV?.users;
+}
+
+
