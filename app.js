a12738b9d52d88c855412d38e30fb707393b683e
BluePanda <m.saratzidis@gmail.com>
2023-06-17 18:13:19 +0200

    return nodes

--- app.js
+++ app.js
@@ -23,17 +23,17 @@ console.log("I am alive!");
 let max_num_updates = 1;
 let posUpdate = 0;
 
-// // --------------- repeatCheckRecalculateNodes ----------------
-// var speedFast_CheckNodes = 2500;
-// var speedSlow_CheckNodes = 6000;
+// --------------- repeatCheckRecalculateNodes ----------------
+var speedFast_CheckNodes = 2500;
+var speedSlow_CheckNodes = 6000;
 
-// var speed_CheckNodes = speedFast_CheckNodes;
-// var speedBefore_CheckNodes = speedFast_CheckNodes;
-// var changeSpeed_CheckNodes = speedFast_CheckNodes;
+var speed_CheckNodes = speedFast_CheckNodes;
+var speedBefore_CheckNodes = speedFast_CheckNodes;
+var changeSpeed_CheckNodes = speedFast_CheckNodes;
 
 
-// let repeatCheckRecalculateNodesVar = setInterval(repeatCheckRecalculateNodes, speed_CheckNodes);
-// // --------------- repeatCheckRecalculateNodes ----------------
+let repeatCheckRecalculateNodesVar = setInterval(repeatCheckRecalculateNodes, speed_CheckNodes);
+// --------------- repeatCheckRecalculateNodes ----------------
 
 
 // --------------- repeatCalculateCVsummaryJobsNodesFunc ----------------
